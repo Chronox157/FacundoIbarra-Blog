1c3ef79e4b0307c33f6be79da12cefef5db7a2b0
unknown <facundo.ibarra.c@gmail.com>
2022-03-16 21:24:04 -0300

    Agregar items

--- indice/models.py
+++ indice/models.py
@@ -1,4 +1,6 @@
+from statistics import mode
 from django.db import models
+from django.forms import CharField
 
 class Post(models.Model):
     titulo = models.CharField(max_length=255)
@@ -6,10 +8,14 @@ class Post(models.Model):
     intro = models.TextField()
     body = models.TextField()
     fecha_add = models.DateField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return self.titulo
+
     class Meta:
         ordering = ['-fecha_add']
-        
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     nombre = models.CharField(max_length=255)
@@ -19,3 +25,11 @@ class Comment(models.Model):
     
     class Meta:
         ordering = ['fecha_add']
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    valor = models.IntegerField()
+    #creado = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
